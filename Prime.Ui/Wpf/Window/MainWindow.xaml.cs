65ba992aa17f468c6f2746862d97b41c39ae85c7
hitchhiker <me@franciswest.com>
2017-09-07 14:23:35 +0200

    reference updates

--- Prime.Ui/Wpf/Window/MainWindow.xaml.cs
+++ Prime.Ui/Wpf/Window/MainWindow.xaml.cs
@@ -1,26 +1,4 @@
-﻿using System.ComponentModel;
-using System.IO;
-using System.Threading;
-using System.Windows.Threading;
-using FirstFloor.ModernUI.Presentation;
-using FirstFloor.ModernUI.Windows.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Prime.Core;
-using Ookii.Dialogs.Wpf;
-
+﻿
 namespace prime
 {
     /// <summary>
