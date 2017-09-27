4d7510c6d5da5b3643bd6449f770ece4ae880149
Alyasko <yasko.alexander@gmail.com>
2017-09-27 08:03:39 +0300

    Minor API keys fix.

--- Prime.Plugins/Services/Poloniex/PoloniexAuthenticator.cs
+++ Prime.Plugins/Services/Poloniex/PoloniexAuthenticator.cs
@@ -7,13 +7,6 @@ namespace Prime.Plugins.Services.Poloniex
 {
     public class PoloniexAuthenticator : BaseAuthenticator
     {
-        #region Secrets
-
-        public const String Key = "TRPQNJYF-MMZJ6U8H-BEKIN2ZO-RXKYEH9Y";
-        public const String Secret = "4d286c51c88d4dcc33c5a4e7cb871fd9d8a141002e96b3d72bab5aa9aad52b3eb1baff59fba0fcce169607993227884bd8f46115115b4d7fb77f445ac5dfe0a8";
-
-        #endregion
-
         public PoloniexAuthenticator(ApiKey apiKey) : base(apiKey)
         {
         }
@@ -22,9 +15,9 @@ namespace Prime.Plugins.Services.Poloniex
         {
             var headers = request.Headers;
             var postData = request.Content.ReadAsStringAsync().Result;
-            var sign = HashHMACSHA512Hex(postData, Secret);
+            var sign = HashHMACSHA512Hex(postData, ApiKey.Secret);
 
-            headers.Add("Key", Key);
+            headers.Add("Key", ApiKey.Key);
             headers.Add("Sign", sign);
         }
     }
