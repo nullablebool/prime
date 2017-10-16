2945ba6cb1f0b9fbed0bf0a0e232956640110dea
Sean Caruana <seancaruana@outlook.com>
2017-10-16 23:26:45 +0200

    Managed to get the Star UserControl in the tabs by creating a custom data template for the tab header in AvalonDock. I also created a vm for the Star.xaml, and moved the messenger registering code to this vm so that we can convert it to be vm-agnostic, rather than dependent on the MarketDiscoveryViewModel.

---
+++ Prime.Ui/Wpf/ViewModel/StarViewModel.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Prime.Core;
+using System.Windows.Threading;
+using GalaSoft.MvvmLight.Messaging;
+using Prime.Utility;
+
+namespace Prime.Ui.Wpf.ViewModel
+{
+    public class StarViewModel
+    {
+        //public StarViewModel()
+        //{
+        //    _context = UserContext.Current;
+
+        //    _messenger.Register<BookmarkHasChangedMessage>(this, _context.Token, UpdateStar);
+        //    _messenger.Register<BookmarkStatusResponseMessage>(this, _context.Token, UpdateStar);
+
+        //    _messenger.Send<BookmarkStatusRequestMessage>(new BookmarkStatusRequestMessage(vm.GetPageCommand()), _context.Token);
+
+        //    ClickBookmarkCommand = new RelayCommand(() =>
+        //    {
+        //           _messenger.Send(new BookmarkSetMessage(vm.GetPageCommand(), !isBookmarkSet), _context.Token);
+        //    });
+        //}
+
+        //private bool isBookmarkSet;
+        //private readonly UserContext _context;
+        //public readonly Dispatcher Dispatcher;
+        //private readonly IMessenger _messenger = DefaultMessenger.I.Default;
+
+        //public RelayCommand ClickBookmarkCommand { get; }
+
+        //private void UpdateStar(BookmarkMessageBase m)
+        //{
+        //    if (!m.Bookmark.Equals(vm.GetPageCommand()))
+        //       return;
+
+        //    Dispatcher.Invoke(() =>
+        //    {
+        //        isBookmarkSet = m.IsBookmarked;
+        //    });
+        //}
+    }
+}
