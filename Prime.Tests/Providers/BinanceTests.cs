7a3bba408d94ce3427b0c7b93e023e039e9b40fd
hitchhiker <me@franciswest.com>
2017-10-21 11:36:28 +0200

    Merge fixes

--- Prime.Tests/Providers/BinanceTests.cs
+++ Prime.Tests/Providers/BinanceTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Common;
 using Prime.Core;
 using Prime.Plugins.Services.Binance;
 
