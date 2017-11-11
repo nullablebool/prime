7e827486800350315755b52c9499a0886b0c9592
hitchhiker <me@franciswest.com>
2017-11-11 09:05:13 +0100

    minor refactor

--- Prime.Common/Exchange/MarketPricesResult.cs
+++ Prime.Common/Exchange/MarketPricesResult.cs
@@ -6,15 +6,8 @@ namespace Prime.Common
 {
     public class MarketPricesResult
     {
-        private AssetPairs _missedPairs;
+        public List<MarketPrice> MarketPrices { get; set; } = new List<MarketPrice>();
 
-        public MarketPricesResult()
-        {
-            MarketPrices = new List<MarketPrice>();
-            _missedPairs = new AssetPairs();
-        }
-
-        public List<MarketPrice> MarketPrices { get; set; }
         public bool IsCompleted => MissedPairs.Count == 0;
 
         public AssetPairs MissedPairs { get; set; } = new AssetPairs();
