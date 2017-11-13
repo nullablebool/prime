72068b8fdc984bc45ce1bc51c8c85b68349461a3
Alyasko <yasko.alexander@gmail.com>
2017-11-13 22:29:11 +0200

    Updated Bittrex tests.

--- Prime.Tests/Providers/BittrexTests.cs
+++ Prime.Tests/Providers/BittrexTests.cs
@@ -21,26 +21,29 @@ namespace Prime.Tests.Providers
         [TestMethod]
         public override async Task TestApiAsync()
         {
-            await base.TestApiAsync();
+            await base.TestApiAsync().ConfigureAwait(false);
         }
 
         [TestMethod]
         public override async Task TestGetAddressesAsync()
         {
-            await base.TestGetAddressesAsync();
+            var context = new WalletAddressContext(UserContext.Current);
+
+            await base.TestGetAddressesAsync(context).ConfigureAwait(false);
         }
 
         [TestMethod]
         public override async Task TestGetAddressesForAssetAsync()
         {
-            WalletAddressAssetContext = new WalletAddressAssetContext("BTC".ToAssetRaw(), UserContext.Current);
-            await base.TestGetAddressesForAssetAsync();
+            var context = new WalletAddressAssetContext("BTC".ToAssetRaw(), UserContext.Current);
+
+            await base.TestGetAddressesForAssetAsync(context).ConfigureAwait(false);
         }
 
         [TestMethod]
         public override async Task TestGetAssetPairsAsync()
         {
-            RequiredAssetPairs = new AssetPairs()
+            var requiredPairs = new AssetPairs()
             {
                 "BTC_LTC".ToAssetPairRaw(),
                 "BTC_XRP".ToAssetPairRaw(),
@@ -48,7 +51,7 @@ namespace Prime.Tests.Providers
                 "BTC_ETC".ToAssetPairRaw(),
             };
 
-            await base.TestGetAssetPairsAsync();
+            await base.TestGetAssetPairsAsync(requiredPairs).ConfigureAwait(false);
         }
 
         [TestMethod]
@@ -67,47 +70,47 @@ namespace Prime.Tests.Providers
         [TestMethod]
         public override async Task TestGetPricesAsync()
         {
-            PublicPricesContext = new PublicPricesContext(new List<AssetPair>()
+            var context = new PublicPricesContext(new List<AssetPair>()
             {
                 "BTC_LTC".ToAssetPairRaw(),
                 "BTC_XRP".ToAssetPairRaw(),
                 "BTC_BCC".ToAssetPairRaw()
             });
 
-            await base.TestGetPricesAsync();
+            await base.TestGetPricesAsync(context).ConfigureAwait(false);
         }
 
         [TestMethod]
         public override async Task TestGetAssetPricesAsync()
         {
-            PublicAssetPricesContext = new PublicAssetPricesContext(new List<Asset>()
+            var context = new PublicAssetPricesContext(new List<Asset>()
             {
                 "ETH".ToAssetRaw(),
                 "BTC".ToAssetRaw()
             }, "WINGS".ToAssetRaw());
 
-            await base.TestGetAssetPricesAsync();
+            await base.TestGetAssetPricesAsync(context).ConfigureAwait(false);
         }
 
         [TestMethod]
         public override async Task TestGetOrderBookAsync()
         {
-            OrderBookContext = new OrderBookContext(new AssetPair("BTC".ToAssetRaw(), "LTC".ToAssetRaw()));
-            await base.TestGetOrderBookAsync();
+            var context = new OrderBookContext(new AssetPair("BTC".ToAssetRaw(), "LTC".ToAssetRaw()));
+            await base.TestGetOrderBookAsync(context).ConfigureAwait(false);
 
-            OrderBookContext = new OrderBookContext(new AssetPair("BTC".ToAssetRaw(), "LTC".ToAssetRaw()), 100);
-            await base.TestGetOrderBookAsync();
+            context = new OrderBookContext(new AssetPair("BTC".ToAssetRaw(), "LTC".ToAssetRaw()), 100);
+            await base.TestGetOrderBookAsync(context).ConfigureAwait(false);
         }
 
         [TestMethod]
         public override async Task TestGetVolumeAsync()
         {
-            VolumeContext = new VolumeContext()
+            var context = new VolumeContext()
             {
                 Pair = "BTC_LTC".ToAssetPairRaw()
             };
 
-            await base.TestGetVolumeAsync();
+            await base.TestGetVolumeAsync(context).ConfigureAwait(false);
         }
     }
 }
