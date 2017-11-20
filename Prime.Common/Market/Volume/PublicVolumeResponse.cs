ee1ca355e0d66e3efe3d6d225aaad03e0eb63e70
hitchhiker <me@franciswest.com>
2017-11-20 13:06:53 +0100

    Merge fixes

--- Prime.Common/Market/Volume/PublicVolumeResponse.cs
+++ Prime.Common/Market/Volume/PublicVolumeResponse.cs
@@ -5,18 +5,18 @@ using Prime.Utility;
 
 namespace Prime.Common
 {
-    public class VolumeCollection
+    public class PublicVolumeResponse
     {
         public readonly IReadOnlyList<NetworkPairVolume> Volume;
         public readonly IReadOnlyDictionary<Network, IReadOnlyList<AssetPair>> Missing;
 
-        public VolumeCollection(IEnumerable<NetworkPairVolume> volume, Network network, IEnumerable<AssetPair> missing)
+        public PublicVolumeResponse(IEnumerable<NetworkPairVolume> volume, Network network, IEnumerable<AssetPair> missing)
         {
             Volume = volume.ToUniqueList();
             Missing = new Dictionary<Network, IReadOnlyList<AssetPair>> {{network, missing.ToUniqueList()}};
         }
 
-        public VolumeCollection(UniqueList<NetworkPairVolume> volume, Dictionary<Network, UniqueList<AssetPair>> missing)
+        public PublicVolumeResponse(UniqueList<NetworkPairVolume> volume, Dictionary<Network, UniqueList<AssetPair>> missing)
         {
             Volume = volume;
             Missing = missing.ToDictionary(x => x.Key, y => y.Value.AsReadOnlyList());
