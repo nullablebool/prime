e2c131403b00a6e5c138dc9b374bba2ff4cb57de
hitchhiker <me@franciswest.com>
2017-11-20 17:03:53 +0100

    public bool CanReturnAll { get; set; }

--- Prime.Common/Prices/Api/Features/Pricing/PricingBulkFeatures.cs
+++ Prime.Common/Prices/Api/Features/Pricing/PricingBulkFeatures.cs
@@ -3,5 +3,7 @@ namespace Prime.Common
     public class PricingBulkFeatures : PricingFeaturesItemBase
     {
         public bool SupportsMultipleQuotes { get; set; } = true;
+
+        public bool CanReturnAll { get; set; }
     }
 }
