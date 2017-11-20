dcb62d5246e15228e498eead4020919f73f19eff
hitchhiker <me@franciswest.com>
2017-11-20 17:01:24 +0100

    Context request all for pairs

--- Prime.Common/Prices/Api/Context/Volume/PublicVolumesContext.cs
+++ Prime.Common/Prices/Api/Context/Volume/PublicVolumesContext.cs
@@ -6,6 +6,8 @@ namespace Prime.Common
 {
     public class PublicVolumesContext : PublicContextBase
     {
+        public PublicVolumesContext(ILogger logger = null) : base(logger) { }
+
         public PublicVolumesContext(IList<AssetPair> pairs, ILogger logger = null) : base(pairs) { }
     }
 }
