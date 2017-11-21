cf342658f200e4c250ea25ce211e5641736334c0
Sean Caruana <seancaruana@outlook.com>
2017-11-21 13:11:36 +0100

    - Implemented ExmoProvider's GetPricingAsync and GetAssetPairsAsync methods - Implemented IExmoApi - Implemented ExmoSchema - Implemented unit tests for Exmo

---
+++ Prime.Plugins/Services/Exmo/ExmoSchema.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime.Plugins.Services.Exmo
+{
+    internal class ExmoSchema
+    {
+        internal class TickerData
+        {
+            public decimal avg;
+            public decimal high;
+            public decimal low;
+            public decimal last_trade;
+            public decimal vol;
+            public decimal vol_curr;
+            public decimal buy_price;
+            public decimal sell_price;
+            public long updated;
+        }
+    }
+}
