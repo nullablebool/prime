8fdfc4895e864c24af019d935cbfb7d45ef5fcac
Sean Caruana <seancaruana@outlook.com>
2017-12-09 23:53:36 +0100

    - Implemented ICoinmateApi - Implemented CoinmateProvider including GetAssetsAsync, public test method and GetPricingAsync - Implemented CoinmateSchema - Implemented Coinmate unit tests - Implemented ICoinsecureApi - Implemented CoinsecureProvider including GetAssetsAsync, public test method and GetPricingAsync - Implemented CoinsecureSchema - Implemented Coinsecure unit tests

---
+++ Prime.Plugins/Services/Coinmate/CoinmateProvider.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+using Prime.Common;
+using Prime.Utility;
+
+namespace Prime.Plugins.Services.Coinmate
+{
+    /// <author email="scaruana_prime@outlook.com">Sean Caruana</author>
+    // https://coinmate.docs.apiary.io/#
+    public class CoinmateProvider : IPublicPricingProvider, IAssetPairsProvider
+    {
+        private const string CoinmateApiUrl = "https://coinmate.io/api/";
+
+        private static readonly ObjectId IdHash = "prime:coinmate".GetObjectIdHashCode();
+
+        //Please not make more than 100 request per minute or we will ban your IP address.
+        //https://coinmate.docs.apiary.io/#reference/request-limits
+        private static readonly IRateLimiter Limiter = new PerMinuteRateLimiter(100, 1);
+
+        private RestApiClientProvider<ICoinmateApi> ApiProvider { get; }
+
+        public Network Network { get; } = Networks.I.Get("Coinmate");
+        private const string PairsCsv = "BTCEUR,BTCCZK";
+
+        public bool Disabled => false;
+        public int Priority => 100;
+        public string AggregatorName => null;
+        public string Title => Network.Name;
+        public ObjectId Id => IdHash;
+        public IRateLimiter RateLimiter => Limiter;
+        public bool IsDirect => true;
+        public char? CommonPairSeparator => '_';
+
+        public ApiConfiguration GetApiConfiguration => ApiConfiguration.Standard2;
+
+        public CoinmateProvider()
+        {
+            ApiProvider = new RestApiClientProvider<ICoinmateApi>(CoinmateApiUrl, this, (k) => null);
+        }
+
+        private AssetPairs _pairs;
+        public AssetPairs Pairs => _pairs ?? (_pairs = new AssetPairs(3, PairsCsv, this));
+
+        public async Task<bool> TestPublicApiAsync(NetworkProviderContext context)
+        {
+            var api = ApiProvider.GetApi(context);
+            var r = await api.GetTickerAsync("BTC_EUR").ConfigureAwait(false);
+
+            return r?.error == false && r.data != null;
+        }
+
+        public Task<AssetPairs> GetAssetPairsAsync(NetworkProviderContext context)
+        {
+            return Task.Run(() => Pairs);
+        }
+
+        public IAssetCodeConverter GetAssetCodeConverter()
+        {
+            return null;
+        }
+
+        private static readonly PricingFeatures StaticPricingFeatures = new PricingFeatures()
+        {
+            Single = new PricingSingleFeatures() { CanStatistics = true, CanVolume = false }
+        };
+
+        public PricingFeatures PricingFeatures => StaticPricingFeatures;
+
+        public async Task<MarketPrices> GetPricingAsync(PublicPricesContext context)
+        {
+            var api = ApiProvider.GetApi(context);
+            var pairCode = context.Pair.ToTicker(this);
+            var r = await api.GetTickerAsync(pairCode).ConfigureAwait(false);
+
+            if (r == null || r.error || r.data == null)
+            {
+                throw new ApiResponseException("No tickers found", this);
+            }
+
+            return new MarketPrices(new MarketPrice(Network, context.Pair, r.data.last)
+            {
+                PriceStatistics = new PriceStatistics(Network, context.Pair.Asset2, r.data.ask, r.data.bid, r.data.low, r.data.high)
+            });
+        }
+    }
+}
---
+++ Prime.Plugins/Services/Coinmate/ICoinmateApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RestEase;
+
+namespace Prime.Plugins.Services.Coinmate
+{
+    internal interface ICoinmateApi
+    {
+        [Get("/ticker?currencyPair={currencyPair}")]
+        Task<CoinmateSchema.TickerResponse> GetTickerAsync([Path] string currencyPair);
+    }
+}
