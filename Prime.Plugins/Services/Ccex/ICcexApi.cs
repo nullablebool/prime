f511fd39a198ced23aadd8f59d0420fea1b0059a
Sean Caruana <seancaruana@outlook.com>
2017-12-11 23:47:03 +0100

    - Implemented ICcexApi - Implemented CcexProvider including GetAssetsAsync, public test method, GetVolumeAsync and GetPricingAsync - Implemented CcexSchema - Implemented CCEX unit tests

---
+++ Prime.Plugins/Services/Ccex/ICcexApi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RestEase;
+
+namespace Prime.Plugins.Services.Ccex
+{
+    internal interface ICcexApi
+    {
+        [Get("/t/{currencyPair}.json")]
+        Task<CcexSchema.TickerResponse> GetTickerAsync([Path(UrlEncode = false)] string currencyPair);
+
+        [Get("/t/prices.json")]
+        Task<CcexSchema.AllTickersResponse> GetTickersAsync();
+
+        [Get("/t/pairs.json")]
+        Task<CcexSchema.AssetPairsResponse> GetAssetPairsAsync();
+
+        [Get("/t/api_pub.html?a=getmarketsummaries")]
+        Task<CcexSchema.VolumeResponse> GetVolumesAsync();
+    }
+}
---
+++ Prime.Tests/Providers/CcexTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Common;
+using Prime.Plugins.Services.Ccex;
+
+namespace Prime.Tests.Providers
+{
+    [TestClass]
+    public class CcexTests : ProviderDirectTestsBase
+    {
+        public CcexTests()
+        {
+            Provider = Networks.I.Providers.OfType<CcexProvider>().FirstProvider();
+        }
+
+        [TestMethod]
+        public override void TestApiPublic()
+        {
+            base.TestApiPublic();
+        }
+
+        [TestMethod]
+        public override void TestGetPricing()
+        {
+            var pairs = new List<AssetPair>()
+            {
+                "USD_BTC".ToAssetPairRaw(),
+                "BTC_USD".ToAssetPairRaw(),
+                "ZNY_DOGE".ToAssetPairRaw()
+            };
+
+            base.TestGetPricing(pairs, true, false);
+        }
+
+        [TestMethod]
+        public override void TestGetAssetPairs()
+        {
+            var requiredPairs = new AssetPairs()
+            {
+                "USD_BTC".ToAssetPairRaw(),
+                "BTC_USD".ToAssetPairRaw(),
+                "ZNY_DOGE".ToAssetPairRaw()
+            };
+
+            base.TestGetAssetPairs(requiredPairs);
+        }
+    }
+}
