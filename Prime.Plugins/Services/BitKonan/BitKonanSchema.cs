70265efc300766d97853c04a5465293ba06750fb
Sean Caruana <seancaruana@outlook.com>
2017-12-12 23:48:10 +0100

    - Implemented IBitKonanApi - Implemented BitKonanProvider including GetAssetsAsync, public test method and GetPricingAsync - Implemented BitKonanSchema - Implemented BitKonan unit tests

---
+++ Prime.Plugins/Services/BitKonan/BitKonanSchema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime.Plugins.Services.BitKonan
+{
+    internal class BitKonanSchema
+    {
+        internal class TickerResponse
+        {
+            public decimal last;
+            public decimal high;
+            public decimal low;
+            public decimal bid;
+            public decimal ask;
+            public decimal open;
+            public decimal volume;
+        }
+    }
+}
---
+++ Prime.Tests/Providers/BitKonanTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Common;
+using Prime.Plugins.Services.BitKonan;
+
+namespace Prime.Tests.Providers
+{
+    [TestClass]
+    public class BitKonanTests : ProviderDirectTestsBase
+    {
+        public BitKonanTests()
+        {
+            Provider = Networks.I.Providers.OfType<BitKonanProvider>().FirstProvider();
+        }
+
+        [TestMethod]
+        public override void TestApiPublic()
+        {
+            base.TestApiPublic();
+        }
+
+        [TestMethod]
+        public override void TestGetPricing()
+        {
+            var pairs = new List<AssetPair>()
+            {
+                "BTC_USD".ToAssetPairRaw(),
+                "LTC_USD".ToAssetPairRaw()
+            };
+
+            base.TestGetPricing(pairs, false);
+        }
+
+        [TestMethod]
+        public override void TestGetAssetPairs()
+        {
+            var requiredPairs = new AssetPairs()
+            {
+                "BTC_USD".ToAssetPairRaw(),
+                "LTC_USD".ToAssetPairRaw()
+            };
+
+            base.TestGetAssetPairs(requiredPairs);
+        }
+    }
+}
