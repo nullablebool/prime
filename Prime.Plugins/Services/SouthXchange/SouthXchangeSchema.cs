7f3878f000d6027d1b26a00b4505ebd48d0cceaf
Sean Caruana <seancaruana@outlook.com>
2017-12-19 23:16:32 +0100

    - Implemented ISouthXchangeApi - Implemented SouthXchangeProvider including GetAssetsAsync, public test method and GetPricingAsync. - Implemented SouthXchangeSchema - Implemented SouthXchange unit tests

---
+++ Prime.Plugins/Services/SouthXchange/SouthXchangeSchema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prime.Plugins.Services.SouthXchange
+{
+    internal class SouthXchangeSchema
+    {
+        internal class AllTickerResponse
+        {
+            public string Market;
+            public decimal? Bid;
+            public decimal? Ask;
+            public decimal? Last;
+            public decimal? Variation24Hr;
+            public decimal? Volume24Hr;
+        }
+
+        internal class TickerResponse
+        {
+            public decimal? Bid;
+            public decimal? Ask;
+            public decimal? Last;
+            public decimal? Variation24Hr;
+            public decimal? Volume24Hr;
+        }
+    }
+}
