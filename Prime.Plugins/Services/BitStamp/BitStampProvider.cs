03c8eb4a1aedd2944a60b68baa67c7b3109736fc
Alyasko <yasko.alexander@gmail.com>
2017-12-21 11:41:42 +0200

    Bitstamp minor updates.

--- Prime.Plugins/Services/BitStamp/BitStampProvider.cs
+++ Prime.Plugins/Services/BitStamp/BitStampProvider.cs
@@ -30,7 +30,7 @@ namespace Prime.Plugins.Services.BitStamp
         public ObjectId Id => IdHash;
         public IRateLimiter RateLimiter => Limiter;
         public bool IsDirect => true;
-        public char? CommonPairSeparator { get; }
+        public char? CommonPairSeparator => null;
 
         public bool CanGenerateDepositAddress => false;
         public bool CanPeekDepositAddress => false;
@@ -75,7 +75,7 @@ namespace Prime.Plugins.Services.BitStamp
         public async Task<MarketPrices> GetPricingAsync(PublicPricesContext context)
         {
             var api = ApiProvider.GetApi(context);
-            var r = await api.GetTickerAsync(context.Pair.ToTicker(this, "").ToLower()).ConfigureAwait(false);
+            var r = await api.GetTickerAsync(context.Pair.ToTicker(this).ToLower()).ConfigureAwait(false);
 
             var price = new MarketPrice(Network, context.Pair, r.last)
             {
@@ -183,7 +183,7 @@ namespace Prime.Plugins.Services.BitStamp
         public async Task<OrderBook> GetOrderBookAsync(OrderBookContext context)
         {
             var api = ApiProvider.GetApi(context);
-            var pairCode = context.Pair.ToTicker(this, "").ToLower();
+            var pairCode = context.Pair.ToTicker(this).ToLower();
 
             var r = await api.GetOrderBookAsync(pairCode).ConfigureAwait(false);
             var orderBook = new OrderBook(Network, context.Pair);
@@ -230,7 +230,7 @@ namespace Prime.Plugins.Services.BitStamp
         public async Task<PublicVolumeResponse> GetPublicVolumeAsync(PublicVolumesContext context)
         {
             var api = ApiProvider.GetApi(context);
-            var r = await api.GetTickerAsync(context.Pair.ToTicker(this, "").ToLower()).ConfigureAwait(false);
+            var r = await api.GetTickerAsync(context.Pair.ToTicker(this).ToLower()).ConfigureAwait(false);
 
             return new PublicVolumeResponse(Network, context.Pair, r.volume);
         }
