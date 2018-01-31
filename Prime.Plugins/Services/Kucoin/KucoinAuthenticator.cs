fe4acf0353b7f13ceb317127f4f9eb5374c6cdb2
Alyasko <yasko.alexander@gmail.com>
2018-01-31 21:42:10 +0200

    Updated Kucoin authenticator.

--- Prime.Plugins/Services/Kucoin/KucoinAuthenticator.cs
+++ Prime.Plugins/Services/Kucoin/KucoinAuthenticator.cs
@@ -22,33 +22,27 @@ namespace Prime.Plugins.Services.Kucoin
             var headers = request.Headers;
             var timeStamp = (long)(DateTime.UtcNow.ToUnixTimeStamp() * 1000); // Milliseconds.
             
-            string endpoint = request.RequestUri.AbsolutePath;
+            var endpoint = request.RequestUri.AbsolutePath;
 
             //Arrange the parameters in ascending alphabetical order (lower cases first), then combine them with & (don't urlencode them, don't add ?, don't add extra &), e.g. amount=10&price=1.1&type=BUY 
-            string parameters = request.RequestUri.Query.Replace("?", "");
-            string[] arrParameters = parameters.Split('&');
+            var parameters = request.RequestUri.Query.Replace("?", "");
+            var arrParameters = parameters.Split('&');
 
             Array.Sort(arrParameters); //Sorts array alphabetically.
 
-            string queryString = string.Join("&", arrParameters);
+            var queryString = string.Join("&", arrParameters);
             
             //splice string for signing
-            string strForSign = endpoint + "/" + timeStamp + "/" + queryString;
+            var strForSign = $"{endpoint}/{timeStamp}/{queryString}";
 
             //Make a base64 encoding of the completed string
-            string signatureStr = Base64Encode(strForSign);
+            var signatureStr = ToBase64(strForSign);
 
             var signature = HashHMACSHA256Hex(signatureStr, ApiKey.Secret);
 
             headers.Add("KC-API-KEY", ApiKey.Key);
             headers.Add("KC-API-NONCE", timeStamp.ToString());
             headers.Add("KC-API-SIGNATURE", signature);
-        }
-
-        public static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
         }
     }
 }
